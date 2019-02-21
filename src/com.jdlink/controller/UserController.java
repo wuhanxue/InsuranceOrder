fd420302ccb308d61552bb5153d5884a8e97e08a
wuhanxue <wuhanxue5@sina.com>
2019-02-21 14:22:33 +0800

    登陆功能

--- src/com.jdlink/controller/UserController.java
+++ src/com.jdlink/controller/UserController.java
@@ -1,4 +1,68 @@
 package com.jdlink.controller;
 
+import com.jdlink.domain.User;
+import com.jdlink.service.UserService;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    /**
+     * 验证登录账号和密码
+     * @param user
+     * @param session
+     * @param request
+     * @return
+     */
+//    @RequestMapping("CheckUserInfo")
+//    @ResponseBody
+    @RequestMapping(value="/CheckUserInfo",method=RequestMethod.POST)
+    public ModelAndView getUser(User user, HttpSession session, HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        try {
+            // 查询参数
+            if (user.getUserName().equals("") || user.getPassword().equals("")) {
+                mav.addObject("status", "fail");
+                mav.addObject("message","账号或用户名为空！");
+            }
+            List<User> userList = userService.getUserByUserNameAndPassword(user);
+            // 更新用户，通过数据查询后得到的用户为准
+            if (userList.size() > 0) {
+                user = userList.get(0);
+                session.setAttribute("user", user); // 将登陆账户存储到session中
+                // 放入转发参数
+                mav.addObject("status", "success");
+                mav.addObject("message","登录成功！");
+                // 放入jsp路径
+                mav.setViewName("/orderList");
+            } else {
+                mav.addObject("status", "fail");
+                mav.addObject("message","账号或密码错误！");
+                mav.setViewName("/signin");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            mav.addObject("status", "fail");
+            mav.addObject("message","服务器异常！");
+            mav.setViewName("/signin");
+        }
+        return mav;
+    }
 }
